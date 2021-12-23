2554d076e9d2415f80b4e9c467be9b0eb33a37e4
Jorge Martinez <jorgemswork@gmail.com>
2021-12-23 22:21:15 +0100

    task3

---
+++ app/BMI.py
@@ -0,0 +1,5 @@
+def BodyMassIndex(height, weight):
+    
+    
+    
+    return (weight / (height*height)) * 703
---
+++ app/flaskapp.py
@@ -0,0 +1,35 @@
+#Import the function to do the calculations
+from app.BMI import BodyMassIndex
+
+#jsonify: Creates a Response with the JSON representation
+from flask import Flask,jsonify, request
+
+#instatiate flask object
+
+app=Flask('__name__')
+
+@app.route('/', methods=['GET', 'POST'])
+def get_input():
+    '''
+    A function to get request using flask, evaluate and return results
+    '''
+    packet = request.get_json(force=True)
+    height = packet['height']
+    weight = packet['weight']
+ 
+
+    
+
+    BMI_Result = BodyMassIndex(height, weight)
+
+    return jsonify(packet, BMI_Result)
+
+# driver function
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=4000)
+    
+
+    
+
